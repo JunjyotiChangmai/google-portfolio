e7bc4b83f875a1ccb8775e0d8d9cce9fc33a175f
Junjyoti Changmai <junjyotichangmai12@gmail.com>
2023-12-27 19:15:48 +0530

    script.js added

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("junResume").onclick = function () { resumeDownl() };
+function resumeDownl() {
+    console.log("resume download clicked");
+}
+
+function searchErase() {
+    document.getElementById("searchBar").value = null;
+}
